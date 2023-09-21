07b6a46d67be60b9db11bd2f0deee34802a4fafb
zhangchongjie <zhangchongjie@risesoft.net>
2023-09-21 10:56:17 +0800

    修改

--- webapp/risenet-y9boot-webapp-processAdmin/src/main/java/net/risesoft/config/ProcessAdminConfiguraton.java
+++ webapp/risenet-y9boot-webapp-processAdmin/src/main/java/net/risesoft/config/ProcessAdminConfiguraton.java
@@ -1,15 +1,7 @@
 package net.risesoft.config;
 
-import jakarta.annotation.PostConstruct;
 import javax.sql.DataSource;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.flowable.ui.modeler.properties.FlowableModelerAppProperties;
-import org.flowable.ui.modeler.service.FlowableModelQueryService;
-import org.flowable.ui.modeler.service.ModelImageService;
-import org.flowable.ui.modeler.service.ModelServiceImpl;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -18,14 +10,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.filter.OrderedRequestContextFilter;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.filter.RequestContextFilter;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -35,13 +23,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.zaxxer.hikari.HikariDataSource;
 
 import net.risesoft.filter.ProcessAdminCheckUserLoginFilter;
-import net.risesoft.filter.RemoveUrlJsessionIdFilter;
 import net.risesoft.listener.FlowableMultiTenantListener;
 import net.risesoft.y9.Y9Context;
 import net.risesoft.y9.configuration.Y9Properties;
 import net.risesoft.y9.configuration.feature.sso.Y9SsoClientProperties;
 import net.risesoft.y9.tenant.datasource.Y9TenantDataSource;
 import net.risesoft.y9.tenant.datasource.Y9TenantDataSourceLookup;
+
+import jakarta.annotation.PostConstruct;
 
 /**
  * @author qinman
@@ -50,153 +39,102 @@ import net.risesoft.y9.tenant.datasource.Y9TenantDataSourceLookup;
  */
 @Configuration
 @EnableConfigurationProperties(Y9Properties.class)
-@ImportResource({"classpath:/springconfigs/flowable.cfg.xml"})
-@ComponentScan(basePackages = {"net.risesoft", "org.flowable.ui"})
+@ImportResource({ "classpath:/springconfigs/flowable.cfg.xml" })
+@ComponentScan(basePackages = { "net.risesoft"})
 public class ProcessAdminConfiguraton implements WebMvcConfigurer {
 
-    /**
-     * 
-     * Description: starter-log工程用到了RequestContextHolder https://github.com/spring-projects/spring-boot/issues/2637
-     * https://github.com/spring-projects/spring-boot/issues/4331
-     * 
-     * @return
-     */
-    @Bean
-    public static RequestContextFilter requestContextFilter() {
-        return new OrderedRequestContextFilter();
-    }
+	/**
+	 *
+	 * Description: starter-log工程用到了RequestContextHolder
+	 * https://github.com/spring-projects/spring-boot/issues/2637
+	 * https://github.com/spring-projects/spring-boot/issues/4331
+	 *
+	 * @return
+	 */
+	@Bean
+	public static RequestContextFilter requestContextFilter() {
+		return new OrderedRequestContextFilter();
+	}
 
-    @Autowired
-    private Environment environment;
+	@Autowired
+	private Environment environment;
 
-    @Autowired
-    Y9Properties y9config;
+	@Autowired
+	Y9Properties y9config;
 
-    Y9SsoClientProperties configProps;
+	Y9SsoClientProperties configProps;
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/", "/main/index");
-    }
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "/main/index");
+	}
 
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
-    }
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
 
-    @Bean
-    public FlowableModelerAppProperties flowableModelerAppProperties() {
-        return new FlowableModelerAppProperties();
-    }
+	@Bean
+	public FlowableMultiTenantListener flowableMultiTenantListener() {
+		return new FlowableMultiTenantListener();
+	}
 
-    @Bean
-    public FlowableModelQueryService flowableModelQueryService() {
-        return new FlowableModelQueryService();
-    }
+	@PostConstruct
+	public void init() {
+		configProps = y9config.getFeature().getSso();
+	}
 
-    @Bean
-    public FlowableMultiTenantListener flowableMultiTenantListener() {
-        return new FlowableMultiTenantListener();
-    }
+	@Bean(name = { "jdbcTemplate4Public" })
+	@ConditionalOnMissingBean(name = "jdbcTemplate4Public")
+	public JdbcTemplate jdbcTemplate4Public(@Qualifier("y9PublicDS") HikariDataSource y9PublicDs) {
+		return new JdbcTemplate(y9PublicDs);
+	}
 
-    @PostConstruct
-    public void init() {
-        configProps = y9config.getFeature().getSso();
-    }
+	@Bean("jdbcTemplate4Tenant")
+	@ConditionalOnMissingBean(name = "jdbcTemplate4Tenant")
+	public JdbcTemplate jdbcTemplate4Tenant(@Qualifier("y9TenantDataSource") DataSource ds) {
+		return new JdbcTemplate(ds);
+	}
 
-    @Bean(name = {"jdbcTemplate4Public"})
-    @ConditionalOnMissingBean(name = "jdbcTemplate4Public")
-    public JdbcTemplate jdbcTemplate4Public(@Qualifier("y9PublicDS") HikariDataSource y9PublicDs) {
-        return new JdbcTemplate(y9PublicDs);
-    }
+	@Bean
+	public FilterRegistrationBean<ProcessAdminCheckUserLoginFilter> processAdminCheckUserLoginFilter() {
+		final FilterRegistrationBean<ProcessAdminCheckUserLoginFilter> filterBean = new FilterRegistrationBean<ProcessAdminCheckUserLoginFilter>();
+		filterBean.setFilter(new ProcessAdminCheckUserLoginFilter());
+		filterBean.setAsyncSupported(false);
+		filterBean.setOrder(50);
+		filterBean.addUrlPatterns("/*");
+		return filterBean;
+	}
 
-    @Bean("jdbcTemplate4Tenant")
-    @ConditionalOnMissingBean(name = "jdbcTemplate4Tenant")
-    public JdbcTemplate jdbcTemplate4Tenant(@Qualifier("y9TenantDataSource") DataSource ds) {
-        return new JdbcTemplate(ds);
-    }
+	@Bean
+	public Y9Context y9Context() {
+		return new Y9Context();
+	}
 
-    @Bean
-    public ModelImageService modelImageService() {
-        return new ModelImageService();
-    }
+	@Primary
+	@ConfigurationProperties("spring.datasource.hikari.flowable")
+	@Bean(name = { "y9FlowableDS" })
+	public HikariDataSource y9FlowableDs() {
+		HikariDataSource dataSource = new HikariDataSource();
+		return dataSource;
+	}
 
-    @Bean
-    public ModelServiceImpl modelService() {
-        return new ModelServiceImpl();
-    }
+	@ConfigurationProperties("spring.datasource.hikari.y9-public")
+	@Bean(name = { "y9PublicDS" })
+	@ConditionalOnMissingBean(name = "y9PublicDS")
+	public HikariDataSource y9PublicDs() {
+		HikariDataSource dataSource = new HikariDataSource();
+		return dataSource;
+	}
 
-    @Bean
-    public FilterRegistrationBean<ProcessAdminCheckUserLoginFilter> processAdminCheckUserLoginFilter() {
-        final FilterRegistrationBean<ProcessAdminCheckUserLoginFilter> filterBean =
-            new FilterRegistrationBean<ProcessAdminCheckUserLoginFilter>();
-        filterBean.setFilter(new ProcessAdminCheckUserLoginFilter());
-        filterBean.setAsyncSupported(false);
-        filterBean.setOrder(50);
-        filterBean.addUrlPatterns("/*");
-        return filterBean;
-    }
+	@Bean("y9TenantDataSource")
+	public DataSource y9TenantDataSource(@Qualifier("y9FlowableDS") HikariDataSource y9FlowableDs,
+		@Qualifier("y9TenantDataSourceLookup") Y9TenantDataSourceLookup y9TenantDataSourceLookup) {
+		return new Y9TenantDataSource(y9FlowableDs, y9TenantDataSourceLookup);
+	}
 
-    @Bean
-    public FilterRegistrationBean<RemoveUrlJsessionIdFilter> removeUrlJsessionIdFilter() {
-        final FilterRegistrationBean<RemoveUrlJsessionIdFilter> filterBean =
-            new FilterRegistrationBean<RemoveUrlJsessionIdFilter>();
-        filterBean.setFilter(new RemoveUrlJsessionIdFilter());
-        filterBean.setAsyncSupported(false);
-        filterBean.addUrlPatterns("/*");
-        if (this.configProps.getSingleSignOutFilterOrder() != null) {
-            filterBean.setOrder(this.configProps.getSingleSignOutFilterOrder());
-        } else {
-            filterBean.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
-        }
-        return filterBean;
-    }
-
-    @Bean(name = {"sqlSessionFactory"})
-    public SqlSessionFactory sqlSessionFactory(@Qualifier("y9FlowableDS") HikariDataSource dataSource) throws Exception {
-        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-        bean.setDataSource(dataSource);
-
-        Resource resource =
-            new PathMatchingResourcePatternResolver().getResource("classpath:mybatis/mybatis-config.xml");
-        bean.setConfigLocation(resource);
-        return bean.getObject();
-    }
-
-    @Bean(name = {"flowableModeler"})
-    public SqlSessionTemplate sqlSessionTemplate(@Qualifier("sqlSessionFactory") SqlSessionFactory sqlSessionFactory) {
-        return new SqlSessionTemplate(sqlSessionFactory);
-    }
-
-    @Bean
-    public Y9Context y9Context() {
-        return new Y9Context();
-    }
-
-    @Primary
-    @ConfigurationProperties("spring.datasource.hikari.flowable")
-    @Bean(name = {"y9FlowableDS"})
-    public HikariDataSource y9FlowableDs() {
-    	HikariDataSource dataSource = new HikariDataSource();
-        return dataSource;
-    }
-
-    @ConfigurationProperties("spring.datasource.hikari.y9-public")
-    @Bean(name = {"y9PublicDS"})
-    @ConditionalOnMissingBean(name = "y9PublicDS")
-    public HikariDataSource y9PublicDs() {
-    	HikariDataSource dataSource = new HikariDataSource();
-        return dataSource;
-    }
-
-    @Bean("y9TenantDataSource")
-    public DataSource y9TenantDataSource(@Qualifier("y9FlowableDS") HikariDataSource y9FlowableDs,
-        @Qualifier("y9TenantDataSourceLookup") Y9TenantDataSourceLookup y9TenantDataSourceLookup) {
-        return new Y9TenantDataSource(y9FlowableDs, y9TenantDataSourceLookup);
-    }
-
-    @Bean("y9TenantDataSourceLookup")
-    public Y9TenantDataSourceLookup y9TenantDataSourceLookup(@Qualifier("y9PublicDS") HikariDataSource ds) {
-        return new Y9TenantDataSourceLookup(ds, environment.getProperty("y9.systemName"));
-    }
+	@Bean("y9TenantDataSourceLookup")
+	public Y9TenantDataSourceLookup y9TenantDataSourceLookup(@Qualifier("y9PublicDS") HikariDataSource ds) {
+		return new Y9TenantDataSourceLookup(ds, environment.getProperty("y9.systemName"));
+	}
 }
