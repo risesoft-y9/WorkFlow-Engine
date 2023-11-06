3735be8c3c12496ba768d164da8aa99cdc895be5
qinman <qinman@risesoft.net>
2023-11-06 11:21:41 +0800

    添加“提交”按钮相关代码

--- support/risenet-y9boot-support-itemAdmin-position/src/main/java/net/risesoft/service/form/impl/Y9FormServiceImpl.java
+++ support/risenet-y9boot-support-itemAdmin-position/src/main/java/net/risesoft/service/form/impl/Y9FormServiceImpl.java
@@ -310,7 +310,7 @@ public class Y9FormServiceImpl implements Y9FormService {
                         List<Y9TableField> tableFieldList =
                             y9TableFieldRepository.findByTableIdOrderByDisplayOrderAsc(y9Table.getId());
                         for (Y9TableField tableField : tableFieldList) {
-                            if (1 == tableField.getIsVar()) {
+                            if (null != tableField.getIsVar() && 1 == tableField.getIsVar()) {
                                 String fieldName = tableField.getFieldName();
                                 map.put(fieldName, datamap.get(0).get(fieldName) != null
                                     ? datamap.get(0).get(fieldName).toString() : "");
