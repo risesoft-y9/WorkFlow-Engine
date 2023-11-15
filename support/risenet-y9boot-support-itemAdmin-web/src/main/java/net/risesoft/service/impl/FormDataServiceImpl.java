1d784ab2096fe17b60de945e06eeb0f095a4acb8
zhangchongjie <zhangchongjie@risesoft.net>
2023-11-15 15:38:39 +0800

    表单字段权限优化

--- support/risenet-y9boot-support-itemAdmin-web/src/main/java/net/risesoft/service/impl/FormDataServiceImpl.java
+++ support/risenet-y9boot-support-itemAdmin-web/src/main/java/net/risesoft/service/impl/FormDataServiceImpl.java
@@ -96,8 +96,7 @@ public class FormDataServiceImpl implements FormDataService {
     }
 
     @Override
-    public List<Map<String, Object>> getChildTableData(String formId, String tableId, String processSerialNumber)
-        throws Exception {
+    public List<Map<String, Object>> getChildTableData(String formId, String tableId, String processSerialNumber) throws Exception {
         return y9FormService.getChildTableData(formId, tableId, processSerialNumber);
     }
 
@@ -107,10 +106,8 @@ public class FormDataServiceImpl implements FormDataService {
         try {
             SpmApproveItem item = spmApproveItemService.findById(itemId);
             String processDefineKey = item.getWorkflowGuid();
-            ProcessDefinitionModel processDefinition =
-                repositoryManager.getLatestProcessDefinitionByKey(tenantId, processDefineKey);
-            List<Y9FormItemBind> formList =
-                y9FormItemBindService.findByItemIdAndProcDefIdAndTaskDefKeyIsNull(itemId, processDefinition.getId());
+            ProcessDefinitionModel processDefinition = repositoryManager.getLatestProcessDefinitionByKey(tenantId, processDefineKey);
+            List<Y9FormItemBind> formList = y9FormItemBindService.findByItemIdAndProcDefIdAndTaskDefKeyIsNull(itemId, processDefinition.getId());
             List<Map<String, Object>> list = null;
             for (Y9FormItemBind bind : formList) {
                 String formId = bind.getFormId();
@@ -118,8 +115,7 @@ public class FormDataServiceImpl implements FormDataService {
                 for (String tableName : tableNameList) {
                     Y9Table y9Table = y9TableService.findByTableName(tableName);
                     if (y9Table.getTableType() == 1) {
-                        list = jdbcTemplate.queryForList("SELECT * FROM " + tableName.toUpperCase() + " WHERE GUID=?",
-                            processSerialNumber);
+                        list = jdbcTemplate.queryForList("SELECT * FROM " + tableName.toUpperCase() + " WHERE GUID=?", processSerialNumber);
                         if (list.size() > 0) {
                             retMap.putAll(list.get(0));
                         }
@@ -134,16 +130,14 @@ public class FormDataServiceImpl implements FormDataService {
     }
 
     @Override
-    public Map<String, Object> getFieldPerm(String formId, String fieldName, String taskDefKey,
-        String processDefinitionId) {
+    public Map<String, Object> getFieldPerm(String formId, String fieldName, String taskDefKey, String processDefinitionId) {
         String tenantId = Y9LoginUserHolder.getTenantId();
         Y9LoginUserHolder.setTenantId(tenantId);
         Map<String, Object> resMap = new HashMap<String, Object>(16);
         resMap.put("writePerm", false);
         resMap.put("fieldName", fieldName);
         try {
-            Y9FieldPerm y9FieldPerm = y9FieldPermRepository.findByFormIdAndFieldNameAndProcessDefinitionIdAndTaskDefKey(
-                formId, fieldName, processDefinitionId, taskDefKey);
+            Y9FieldPerm y9FieldPerm = y9FieldPermRepository.findByFormIdAndFieldNameAndTaskDefKey(formId, fieldName, taskDefKey);
             if (y9FieldPerm != null) {
                 resMap.putAll(getFieldPerm(y9FieldPerm));
             } else {
@@ -191,10 +185,8 @@ public class FormDataServiceImpl implements FormDataService {
         try {
             SpmApproveItem item = spmApproveItemService.findById(itemId);
             String processDefineKey = item.getWorkflowGuid();
-            ProcessDefinitionModel processDefinition =
-                repositoryManager.getLatestProcessDefinitionByKey(Y9LoginUserHolder.getTenantId(), processDefineKey);
-            List<Y9FormItemBind> formList =
-                y9FormItemBindService.findByItemIdAndProcDefIdAndTaskDefKeyIsNull(itemId, processDefinition.getId());
+            ProcessDefinitionModel processDefinition = repositoryManager.getLatestProcessDefinitionByKey(Y9LoginUserHolder.getTenantId(), processDefineKey);
+            List<Y9FormItemBind> formList = y9FormItemBindService.findByItemIdAndProcDefIdAndTaskDefKeyIsNull(itemId, processDefinition.getId());
             for (Y9FormItemBind form : formList) {
                 List<Y9FormField> formElementList = y9FormFieldService.findByFormId(form.getFormId());
                 for (Y9FormField formElement : formElementList) {
@@ -262,8 +254,7 @@ public class FormDataServiceImpl implements FormDataService {
     }
 
     @Override
-    public void saveChildTableData(String formId, String tableId, String processSerialNumber, String jsonData)
-        throws Exception {
+    public void saveChildTableData(String formId, String tableId, String processSerialNumber, String jsonData) throws Exception {
         try {
             Map<String, Object> map = new HashMap<String, Object>(16);
             map = y9FormService.saveChildTableData(formId, tableId, processSerialNumber, jsonData);
