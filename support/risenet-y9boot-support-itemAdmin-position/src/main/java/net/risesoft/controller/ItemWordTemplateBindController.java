3ec59360bc843326f1f161b6f4b18f50efd29b33
zhangchongjie <zhangchongjie@risesoft.net>
2023-12-21 15:02:46 +0800

    bug修复
    (cherry picked from commit 746bec5305770c8f96c244c2cdf1f83c871a17e9)

--- support/risenet-y9boot-support-itemAdmin-position/src/main/java/net/risesoft/controller/ItemWordTemplateBindController.java
+++ support/risenet-y9boot-support-itemAdmin-position/src/main/java/net/risesoft/controller/ItemWordTemplateBindController.java
@@ -10,14 +10,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import net.risesoft.api.org.PersonApi;
 import net.risesoft.api.processadmin.RepositoryApi;
 import net.risesoft.consts.UtilConsts;
 import net.risesoft.entity.ItemWordTemplateBind;
 import net.risesoft.entity.SpmApproveItem;
 import net.risesoft.entity.WordTemplate;
 import net.risesoft.model.processadmin.ProcessDefinitionModel;
-import net.risesoft.model.user.UserInfo;
 import net.risesoft.pojo.Y9Result;
 import net.risesoft.service.ItemWordTemplateBindService;
 import net.risesoft.service.SpmApproveItemService;
@@ -45,9 +43,6 @@ public class ItemWordTemplateBindController {
     @Autowired
     private RepositoryApi repositoryManager;
 
-    @Autowired
-    private PersonApi personManager;
-
     /**
      * 删除正文模板绑定
      *
@@ -72,18 +67,13 @@ public class ItemWordTemplateBindController {
     @RequestMapping(value = "/getTemplateBind", method = RequestMethod.GET, produces = "application/json")
     public Y9Result<Map<String, Object>> getTemplateBind(@RequestParam String itemId) {
         Map<String, Object> map = new HashMap<String, Object>(16);
-        UserInfo person = Y9LoginUserHolder.getUserInfo();
         SpmApproveItem item = spmApproveItemService.findById(itemId);
-        String processDefinitionKey = item.getWorkflowGuid(), tenantId = Y9LoginUserHolder.getTenantId(),
-            personId = person.getPersonId();
-        ProcessDefinitionModel processDefinition =
-            repositoryManager.getLatestProcessDefinitionByKey(tenantId, processDefinitionKey);
+        String processDefinitionKey = item.getWorkflowGuid(), tenantId = Y9LoginUserHolder.getTenantId();
+        ProcessDefinitionModel processDefinition = repositoryManager.getLatestProcessDefinitionByKey(tenantId, processDefinitionKey);
         String processDefinitionId = processDefinition.getId();
         map.put("processDefinitionId", processDefinitionId);
-        List<WordTemplate> templateList = wordTemplateService
-            .findByBureauIdOrderByUploadTimeDesc(personManager.getBureau(tenantId, personId).getData().getId());
-        ItemWordTemplateBind wordTemplateBind =
-            itemWordTemplateBindService.findByItemIdAndProcessDefinitionId(itemId, processDefinitionId);
+        List<WordTemplate> templateList = wordTemplateService.findAll();
+        ItemWordTemplateBind wordTemplateBind = itemWordTemplateBindService.findByItemIdAndProcessDefinitionId(itemId, processDefinitionId);
         String tempName = "", bindId = "";
         if (wordTemplateBind != null) {
             for (WordTemplate wordTemplate : templateList) {
@@ -108,8 +98,7 @@ public class ItemWordTemplateBindController {
      * @return
      */
     @RequestMapping(value = "/save", method = RequestMethod.POST, produces = "application/json")
-    public Y9Result<String> save(@RequestParam String itemId, @RequestParam String processDefinitionId,
-        @RequestParam String templateId) {
+    public Y9Result<String> save(@RequestParam String itemId, @RequestParam String processDefinitionId, @RequestParam String templateId) {
         Map<String, Object> map = itemWordTemplateBindService.save(itemId, processDefinitionId, templateId);
         if ((boolean)map.get(UtilConsts.SUCCESS)) {
             return Y9Result.successMsg((String)map.get("msg"));
