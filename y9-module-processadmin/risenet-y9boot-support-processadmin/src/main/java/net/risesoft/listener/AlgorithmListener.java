bd0f2893f63eaa82c309d75752546c5794971303
qinman <qinman@risesoft.net>
2024-03-20 16:46:47 +0800

    y9_public_new --> y9_public
    (cherry picked from commit 0f2aba8f257de5f5a1631a98252f9113dcc239ab)

---
+++ y9-module-processadmin/risenet-y9boot-support-processadmin/src/main/java/net/risesoft/listener/AlgorithmListener.java
@@ -0,0 +1,27 @@
+package net.risesoft.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.ExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 提取
+ *
+ * @author qinman
+ * @date 2024/03/19
+ */
+@Slf4j
+public class AlgorithmListener implements ExecutionListener {
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void notify(DelegateExecution execution) {
+        try {
+            System.out.printf("==========processSerialNumber" + execution.getVariable("processSerialNumber"));
+            System.out.printf("==========processInstanceId" + execution.getProcessInstanceId());
+        } catch (Exception e) {
+            throw new RuntimeException("调用计算接口失败");
+        }
+    }
+}
