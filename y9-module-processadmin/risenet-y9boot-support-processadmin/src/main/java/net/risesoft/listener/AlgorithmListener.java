95c3eddc24120077adaf9d3f5b4ee29d84ac6e48
qinman <qinman@risesoft.net>
2024-04-02 15:52:26 +0800

    完善执行监听器中调用图像计算和发布的接口
    (cherry picked from commit fab2796e95cc2b5e9211d53d34e27676f14ab6b3)

--- y9-module-processadmin/risenet-y9boot-support-processadmin/src/main/java/net/risesoft/listener/AlgorithmListener.java
+++ y9-module-processadmin/risenet-y9boot-support-processadmin/src/main/java/net/risesoft/listener/AlgorithmListener.java
@@ -1,9 +1,24 @@
 package net.risesoft.listener;
 
 import lombok.extern.slf4j.Slf4j;
+import net.risesoft.pojo.Y9Result;
+import net.risesoft.service.FlowableTenantInfoHolder;
+import net.risesoft.y9.Y9Context;
+import net.risesoft.y9.Y9LoginUserHolder;
+import net.risesoft.y9.configuration.Y9Properties;
+import net.risesoft.y9.util.RemoteCallUtil;
+import org.apache.commons.httpclient.NameValuePair;
+import org.flowable.engine.TaskService;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.flowable.engine.delegate.ExecutionListener;
+import org.flowable.task.api.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 提取
@@ -18,10 +33,23 @@ public class AlgorithmListener implements ExecutionListener {
     @Transactional(rollbackFor = Exception.class)
     public void notify(DelegateExecution execution) {
         try {
-            System.out.printf("==========processSerialNumber" + execution.getVariable("processSerialNumber"));
-            System.out.printf("==========processInstanceId" + execution.getProcessInstanceId());
+            String requestUrl = Y9Context.getProperty("y9.app.processAdmin.imageCountUrl") + "/service/rest/imageCount/monoLocate";
+            String tenantId = FlowableTenantInfoHolder.getTenantId();
+            String processSerialNumber = execution.getVariable("processSerialNumber").toString();
+            String processInstanceId = execution.getProcessInstanceId();
+            TaskService taskService=Y9Context.getBean(TaskService.class);
+            List<Task> tasks = taskService.createTaskQuery().processInstanceId(processInstanceId).list();
+            String positionId = tasks.get(0).getAssignee();
+            List<NameValuePair> params = new ArrayList<>();
+            params.add(new NameValuePair("tenantId", tenantId));
+            params.add(new NameValuePair("positionId", positionId));
+            params.add(new NameValuePair("processSerialNumber", processSerialNumber));
+            Y9Result<Boolean> y9Result = RemoteCallUtil.postCallRemoteService(requestUrl, params, Y9Result.class);
+            if (!y9Result.isSuccess()) {
+                throw new RuntimeException("调用计算接口失败：" + y9Result.getMsg());
+            }
         } catch (Exception e) {
-            throw new RuntimeException("调用计算接口失败");
+            throw new RuntimeException("调用计算接口异常");
         }
     }
 }
