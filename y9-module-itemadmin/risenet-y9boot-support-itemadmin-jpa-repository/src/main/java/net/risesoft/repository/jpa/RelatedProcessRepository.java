7f9d7ab2552381174ded2c344ba662237c34f592
yihong <yihong@risesoft.net>
2024-07-10 12:29:26 +0800

    修改关联流程接口排序方式

--- y9-module-itemadmin/risenet-y9boot-support-itemadmin-jpa-repository/src/main/java/net/risesoft/repository/jpa/RelatedProcessRepository.java
+++ y9-module-itemadmin/risenet-y9boot-support-itemadmin-jpa-repository/src/main/java/net/risesoft/repository/jpa/RelatedProcessRepository.java
@@ -1,22 +1,24 @@
 package net.risesoft.repository.jpa;
 
-import net.risesoft.entity.RelatedProcess;
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import net.risesoft.entity.RelatedProcess;
 
 @Transactional(value = "rsTenantTransactionManager", readOnly = true)
-public interface RelatedProcessRepository extends JpaRepository<RelatedProcess, String>, JpaSpecificationExecutor<RelatedProcess> {
-	
-	Page<RelatedProcess> findByParentItemId(String itemId, Pageable pageable);
+public interface RelatedProcessRepository
+    extends JpaRepository<RelatedProcess, String>, JpaSpecificationExecutor<RelatedProcess> {
 
-	List<RelatedProcess> findByParentItemIdAndItemNameLike(String parentItemId,String itemName);
+    Page<RelatedProcess> findByParentItemIdOrderByCreateDateAsc(String itemId, Pageable pageable);
 
-	List<RelatedProcess> findByParentItemId(String parentItemId);
+    List<RelatedProcess> findByParentItemIdAndItemNameLike(String parentItemId, String itemName);
 
-	RelatedProcess findByParentItemIdAndItemId(String parentItemId,String itemId);
+    List<RelatedProcess> findByParentItemId(String parentItemId);
+
+    RelatedProcess findByParentItemIdAndItemId(String parentItemId, String itemId);
 }
