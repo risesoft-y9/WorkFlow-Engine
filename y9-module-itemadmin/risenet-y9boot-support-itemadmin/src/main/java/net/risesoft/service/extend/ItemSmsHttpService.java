21a3d0c830944463ed590f2ee0eabbd7d702070b
zhangchongjie <zhangchongjie@risesoft.net>
2024-10-17 09:41:12 +0800

    添加注解
    (cherry picked from commit f4f1201ac521898298622b7f0e7513f56d88dcc5)

--- y9-module-itemadmin/risenet-y9boot-support-itemadmin/src/main/java/net/risesoft/service/extend/ItemSmsHttpService.java
+++ y9-module-itemadmin/risenet-y9boot-support-itemadmin/src/main/java/net/risesoft/service/extend/ItemSmsHttpService.java
@@ -9,6 +9,16 @@ import java.util.List;
  */
 public interface ItemSmsHttpService {
 
+    /**
+     * 发送短信
+     *
+     * @param tenantId 租户ID
+     * @param userId 用户ID
+     * @param mobile 手机号
+     * @param smsContent 短信内容
+     * @param systemName 系统名称
+     * @throws Exception 异常
+     */
     void sendSmsHttpList(String tenantId, String userId, List<String> mobile, String smsContent, String systemName)
         throws Exception;
 }
