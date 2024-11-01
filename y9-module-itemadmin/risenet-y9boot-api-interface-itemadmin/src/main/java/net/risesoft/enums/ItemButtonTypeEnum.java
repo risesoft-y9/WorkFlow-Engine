8063de6cfba23c22d60334aca66839f84b0edb27
qinman <qinman@risesoft.net>
2024-11-01 15:06:53 +0800

    优化代码
    (cherry picked from commit f5f286e72091ee611bab6db07d88594ac41f3bec)

--- y9-module-itemadmin/risenet-y9boot-api-interface-itemadmin/src/main/java/net/risesoft/enums/ItemButtonTypeEnum.java
+++ y9-module-itemadmin/risenet-y9boot-api-interface-itemadmin/src/main/java/net/risesoft/enums/ItemButtonTypeEnum.java
@@ -13,9 +13,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ItemButtonTypeEnum {
     /** 普通按钮 */
-    COMMON(1, "业务协同"),
+    COMMON(1, "普通按钮"),
     /** 发送按钮 */
-    SEND(2, "事项办理");
+    SEND(2, "发送按钮");
 
     private final Integer value;
     private final String name;
---
+++ y9-module-itemadmin/risenet-y9boot-api-interface-itemadmin/src/main/java/net/risesoft/model/itemadmin/DocumentDetailModel.java
@@ -0,0 +1,158 @@
+package net.risesoft.model.itemadmin;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 流程详情数据
+ *
+ * @author qinman
+ * @date 2024/11/01
+ */
+@Data
+public class DocumentDetailModel implements Serializable {
+
+    private static final long serialVersionUID = 4115564591151087066L;
+    /**
+     * 事项Id
+     */
+    private String itemId;
+
+    /**
+     * 流程编号
+     */
+    private String processSerialNumber;
+
+    /**
+     * 流程定义id
+     */
+    private String processDefinitionId;
+
+    /**
+     * 所属事项绑定的流程定义
+     */
+    private String processDefinitionKey;
+
+    /**
+     * 流程实例
+     */
+    private String processInstanceId;
+
+    /**
+     * 任务key
+     */
+    private String taskDefKey;
+
+    /**
+     * 任务Id
+     */
+    private String taskId;
+
+    /**
+     * 当前岗位名称
+     */
+    private String currentUser;
+
+    /**
+     * 当前人员id
+     */
+    private String activitiUser;
+
+    /**
+     * 状态
+     */
+    private String itembox;
+
+    /**
+     * 表单列表
+     */
+    private List<ItemFormModel> formList;
+
+    /**
+     * 附件，正文，沟通交流页签显示
+     */
+    private String showOtherFlag;
+
+    /**
+     * 打印表单id
+     */
+    private String printFormId;
+
+    /**
+     * 打印表单类型
+     */
+    private String printFormType;
+
+    /**
+     * 重定位选项
+     */
+    private List<Map<String, Object>> repositionMap;
+
+    /**
+     * 重定位选项json数据
+     */
+    private String taskDefNameJson;
+
+    /**
+     * 菜单按钮选项
+     */
+    private List<ItemButtonModel> buttonList;
+
+    /**
+     * 是否主办办理
+     */
+    private String sponsorHandle;
+
+    /**
+     * 拒签时是否是最后一个人员
+     */
+    private boolean isLastPerson4RefuseClaim;
+
+    /**
+     * 任务节点类型
+     */
+    private String multiInstance;
+
+    /**
+     * 是否需要发送下一个节点
+     */
+    private boolean nextNode;
+
+    /**
+     * 是否上会
+     */
+    private boolean meeting;
+
+    /**
+     * 启动节点key
+     */
+    private String startTaskDefKey;
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 启动人id
+     */
+    private String startor;
+
+    /**
+     * 抄送件id
+     */
+    private String id;
+
+    /**
+     * 抄送件状态
+     */
+    private Integer status;
+
+    /**
+     * 是否自定义事项
+     */
+    private boolean customItem;
+}
