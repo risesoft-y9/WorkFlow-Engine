aff1311edb748297990264864aec7a2c8dbf59d3
mengjuhua <mengjuhua@risesoft.net>
2024-11-05 14:00:54 +0800

    chore：修改vue工程依赖插件的版本

--- vue/y9vue-flowableUI/auto-imports.d.ts
+++ vue/y9vue-flowableUI/auto-imports.d.ts
@@ -28,6 +28,7 @@ declare global {
   const ElLoading: typeof import('element-plus/es')['ElLoading']
   const ElMessage: typeof import('element-plus/es')['ElMessage']
   const ElMessageBox: typeof import('element-plus/es')['ElMessageBox']
+  const ElNotification: typeof import('element-plus/es')['ElNotification']
   const extendRef: typeof import('@vueuse/core')['extendRef']
   const getActivePinia: typeof import('pinia')['getActivePinia']
   const getCurrentInstance: typeof import('vue')['getCurrentInstance']
