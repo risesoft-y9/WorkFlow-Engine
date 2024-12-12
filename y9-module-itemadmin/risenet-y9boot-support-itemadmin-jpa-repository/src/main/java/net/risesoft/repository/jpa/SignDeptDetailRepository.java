7ec32a4bfac87bc99c40b89b6b6fd188646b5e76
qinman <qinman@risesoft.net>
2024-12-12 11:44:38 +0800

    ++会签详情
    (cherry picked from commit 8224294279c7c5e2a7974fd650ac42625ae1169c)

---
+++ y9-module-itemadmin/risenet-y9boot-support-itemadmin-jpa-repository/src/main/java/net/risesoft/repository/jpa/SignDeptDetailRepository.java
@@ -0,0 +1,29 @@
+package net.risesoft.repository.jpa;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import net.risesoft.entity.SignDeptDetail;
+
+/**
+ * @author qinman
+ */
+public interface SignDeptDetailRepository
+    extends JpaRepository<SignDeptDetail, String>, JpaSpecificationExecutor<SignDeptDetail> {
+    List<SignDeptDetail> findByProcessSerialNumberAndDeptIdOrderByCreateTimeDesc(String processSerialNumber,
+        String deptId);
+
+    List<SignDeptDetail> findByProcessSerialNumberAndStatusOrderByCreateTimeDesc(String processSerialNumber,
+        int status);
+
+    List<SignDeptDetail> findByProcessInstanceIdAndStatusOrderByCreateTimeDesc(String processInstanceId, int status);
+
+    List<SignDeptDetail> findByTaskIdOrderByCreateTimeDesc(String taskId);
+
+    SignDeptDetail findByExecutionIdAndDeptId(String executionId, String deptId);
+
+    List<SignDeptDetail> findByProcessSerialNumberAndDeptIdAndStatusOrderByCreateTimeDesc(String processSerialNumber,
+        String deptId, int status);
+}
---
+++ y9-module-itemadmin/risenet-y9boot-support-itemadmin/src/main/java/net/risesoft/service/SignDeptDetailService.java
@@ -0,0 +1,18 @@
+package net.risesoft.service;
+
+import java.util.List;
+
+import net.risesoft.entity.SignDeptDetail;
+
+public interface SignDeptDetailService {
+
+    void saveOrUpdate(SignDeptDetail signDeptDetail);
+
+    SignDeptDetail findByProcessSerialNumberAndDeptId4Latest(String processSerialNumber, String deptId);
+
+    List<SignDeptDetail> findByProcessSerialNumberAndDeptId(String processSerialNumber, String deptId);
+
+    List<SignDeptDetail> findByTaskId(String processInstanceId, String taskId);
+
+    List<SignDeptDetail> findByProcessInstanceIdAndStatus(String processInstanceId, int status);
+}
