315113eb62c2b6bc043c711d7cdeccf45854b9ca
mengjuhua <mengjuhua@risesoft.net>
2024-12-24 11:46:03 +0800

    fix：处理合并之后的错误

--- y9-module-itemadmin/risenet-y9boot-support-itemadmin/src/main/java/net/risesoft/service/impl/SignDeptInfoServiceImpl.java
+++ y9-module-itemadmin/risenet-y9boot-support-itemadmin/src/main/java/net/risesoft/service/impl/SignDeptInfoServiceImpl.java
@@ -6,10 +6,10 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
--- y9-module-processadmin/risenet-y9boot-support-processadmin/src/main/java/net/risesoft/service/impl/CustomHistoricTaskServiceImpl.java
+++ y9-module-processadmin/risenet-y9boot-support-processadmin/src/main/java/net/risesoft/service/impl/CustomHistoricTaskServiceImpl.java
@@ -3,7 +3,7 @@ package net.risesoft.service.impl;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.Resource;
+import jakarta.annotation.Resource;
 
 import org.apache.commons.lang3.StringUtils;
 import org.flowable.engine.HistoryService;
