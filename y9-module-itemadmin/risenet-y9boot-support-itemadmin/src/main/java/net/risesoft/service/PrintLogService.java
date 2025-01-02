2237eb2b7b1c9a8d7f7dbf2e0a828d25a0e5207e
zhangchongjie <zhangchongjie@risesoft.net>
2025-01-02 15:39:48 +0800

    打印日志功能
    (cherry picked from commit c1648485de6c5e1216334992301abfd5e07ff0e7)

---
+++ y9-module-itemadmin/risenet-y9boot-support-itemadmin/src/main/java/net/risesoft/service/PrintLogService.java
@@ -0,0 +1,18 @@
+package net.risesoft.service;
+
+import java.util.List;
+
+
+import net.risesoft.entity.PrintLog;
+
+/**
+ * @author qinman
+ * @author zhangchongjie
+ * @date 2022/12/20
+ */
+public interface PrintLogService {
+
+    List<PrintLog> getPrintLogList(String processSerialNumber);
+
+    void savePrintLog(PrintLog log);
+}
