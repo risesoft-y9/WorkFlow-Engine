eaf7f4f63d4ae181048967e539806b6b5ce5e904
qinman <qinman@risesoft.net>
2025-01-13 17:33:43 +0800

    ++传入日期以及天数，计算工作日

--- y9-module-flowableui/risenet-y9boot-webapp-flowableui/src/main/java/net/risesoft/service/WorkDayService.java
+++ y9-module-flowableui/risenet-y9boot-webapp-flowableui/src/main/java/net/risesoft/service/WorkDayService.java
@@ -1,12 +1,26 @@
 package net.risesoft.service;
 
+import java.text.ParseException;
 import java.util.Date;
 
 import net.risesoft.model.itemadmin.TaskRelatedModel;
 
+/**
+ * @author qinman
+ */
 public interface WorkDayService {
 
     int getDay(Date startDate, Date endDate);
+
+    /**
+     * 计算某个日期几个工作日后的日期
+     * 
+     * @param startDate 起始日期
+     * @param days 几个工作日
+     * @return 计算后的日期字符串 YYYY-MM-DD
+     * @throws ParseException
+     */
+    String getDate(Date startDate, int days) throws ParseException;
 
     TaskRelatedModel getLightColor(Date startDate, Date endDate);
 }
