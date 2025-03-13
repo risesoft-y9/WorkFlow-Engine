c76d043e56c9ddba8f426bc36237946ea6cb2fb6
qinman <qinman@risesoft.net>
2025-03-13 17:23:00 +0800

    fix:领导批示功能

---
+++ y9-module-itemadmin/risenet-y9boot-api-feignclient-itemadmin/src/main/java/y9/client/rest/itemadmin/LeaderOpinionApiClient.java
@@ -0,0 +1,17 @@
+package y9.client.rest.itemadmin;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+import net.risesoft.api.itemadmin.LeaderOpinionApi;
+
+/**
+ * @author qinman
+ * @author zhangchongjie
+ * @date 2022/12/19
+ */
+@FeignClient(contextId = "LeaderOpinionApiClient", name = "${y9.service.itemAdmin.name:itemAdmin}",
+    url = "${y9.service.itemAdmin.directUrl:}",
+    path = "/${y9.service.itemAdmin.name:itemAdmin}/services/rest/leaderOpinion")
+public interface LeaderOpinionApiClient extends LeaderOpinionApi {
+
+}
