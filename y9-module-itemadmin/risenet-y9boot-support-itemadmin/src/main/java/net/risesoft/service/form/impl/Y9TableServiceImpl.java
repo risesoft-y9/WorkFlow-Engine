2bdd264393c52c62d2015f1fa60563ed6814a588
qinman <qinman@risesoft.net>
2025-03-25 15:33:44 +0800

    fix:查询bug

--- y9-module-itemadmin/risenet-y9boot-support-itemadmin/src/main/java/net/risesoft/service/form/impl/Y9TableServiceImpl.java
+++ y9-module-itemadmin/risenet-y9boot-support-itemadmin/src/main/java/net/risesoft/service/form/impl/Y9TableServiceImpl.java
@@ -224,7 +224,7 @@ public class Y9TableServiceImpl implements Y9TableService {
                         } else {
                             if (null != searchMap.get(key) && StringUtils.isNotBlank(searchMap.get(key).toString())) {
                                 whereSql.append("AND INSTR(").append(key.toUpperCase()).append(",'")
-                                    .append(searchMap.get(key).toString()).append("') > 0 ");
+                                    .append(searchMap.get(key).toString()).append("') >= 0 ");
                             } else {
                                 whereSql.append("AND (").append(key.toUpperCase()).append("= '' OR ")
                                     .append(key.toUpperCase()).append(" IS NULL)");
