83684c5aeba60f51f174eb4a33f35ed595d0f7c4
qinman <qinman@risesoft.net>
2025-03-26 10:26:35 +0800

    fix:流程启动人不再包含部门id
    (cherry picked from commit f6e62f44258a4dbea77ca967736e3fdfce3c285f)

--- y9-module-itemadmin/risenet-y9boot-support-itemadmin/src/main/java/net/risesoft/service/dynamicrole/impl/StarterDeptSecretary.java
+++ y9-module-itemadmin/risenet-y9boot-support-itemadmin/src/main/java/net/risesoft/service/dynamicrole/impl/StarterDeptSecretary.java
@@ -40,9 +40,8 @@ public class StarterDeptSecretary extends AbstractDynamicRoleMember {
         List<OrgUnit> orgUnitList = new ArrayList<>();
         if (StringUtils.isNotBlank(processInstanceId)) {
             ProcessInstanceModel processInstance = runtimeApi.getProcessInstance(tenantId, processInstanceId).getData();
-            String userIdAndDeptId = processInstance.getStartUserId();
-            if (StringUtils.isNotEmpty(userIdAndDeptId)) {
-                String userId = userIdAndDeptId.split(":")[0];
+            String userId = processInstance.getStartUserId();
+            if (StringUtils.isNotEmpty(userId)) {
                 OrgUnit orgUnit = orgUnitApi.getOrgUnitPersonOrPosition(tenantId, userId).getData();
                 List<OrgUnit> secretary = departmentApi.listDepartmentPropOrgUnits(tenantId, orgUnit.getParentId(),
                     DepartmentPropCategoryEnum.SECRETARY.getValue(), true).getData();
