349e3f426c4be68944ff01918faaedeb52ca97cd
qinman <qinman@risesoft.net>
2025-04-01 15:31:27 +0800

    add: 定时任务
    (cherry picked from commit 971761ec9020066870aa3bcfcf3ed2933f105905)

--- y9-module-flowableui/risenet-y9boot-webapp-flowableui/src/main/java/net/risesoft/FlowableUIApplication.java
+++ y9-module-flowableui/risenet-y9boot-webapp-flowableui/src/main/java/net/risesoft/FlowableUIApplication.java
@@ -2,8 +2,10 @@ package net.risesoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FlowableUIApplication {
     public static void main(String[] args) {
         SpringApplication.run(FlowableUIApplication.class, args);
