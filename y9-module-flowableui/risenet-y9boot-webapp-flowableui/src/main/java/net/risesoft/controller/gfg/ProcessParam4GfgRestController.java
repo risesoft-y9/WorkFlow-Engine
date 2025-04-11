a5ebe9c286ee887dd4f05f10ecfe0f824aa5dab7
zhangchongjie <zhangchongjie@risesoft.net>
2025-04-11 16:00:21 +0800

    更新流程变量主办司局信息
    (cherry picked from commit 755bb618d7ca2a7bea23d163d3c0c6bad4d6eaf2)

--- y9-module-flowableui/risenet-y9boot-webapp-flowableui/src/main/java/net/risesoft/controller/gfg/ProcessParam4GfgRestController.java
+++ y9-module-flowableui/risenet-y9boot-webapp-flowableui/src/main/java/net/risesoft/controller/gfg/ProcessParam4GfgRestController.java
@@ -16,7 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 import net.risesoft.api.itemadmin.ActRuDetailApi;
+import net.risesoft.api.itemadmin.ProcessParamApi;
+import net.risesoft.api.platform.org.OrgUnitApi;
 import net.risesoft.api.processadmin.VariableApi;
+import net.risesoft.model.itemadmin.ProcessParamModel;
 import net.risesoft.model.itemadmin.StartProcessResultModel;
 import net.risesoft.pojo.Y9Result;
 import net.risesoft.service.ProcessParamService;
@@ -35,11 +38,15 @@ import net.risesoft.y9.Y9LoginUserHolder;
 @RequestMapping(value = "/vue/processParam/gfg", produces = MediaType.APPLICATION_JSON_VALUE)
 public class ProcessParam4GfgRestController {
 
+    private final ProcessParamApi processParamApi;
+
     private final ProcessParamService processParamService;
 
     private final VariableApi variableApi;
 
     private final ActRuDetailApi actRuDetailApi;
+
+    private final OrgUnitApi orgUnitApi;
 
     /**
      * 保存流程变量
@@ -90,5 +97,26 @@ public class ProcessParam4GfgRestController {
     @PostMapping(value = "/setRead")
     public Y9Result<Object> setRead(@RequestParam @NotBlank String actRuDetailId) {
         return actRuDetailApi.setRead(Y9LoginUserHolder.getTenantId(), actRuDetailId);
+    }
+
+    /**
+     * 更新主办司局信息
+     *
+     * @param deptId 部门id
+     * @param deptName 部门名称
+     * @param processSerialNumber 流程编号
+     * @return Y9Result<Object>
+     */
+    @PostMapping(value = "/updateHostDept")
+    public Y9Result<Object> updateHostDept(@RequestParam String deptId, @RequestParam String deptName,
+        @RequestParam String processSerialNumber) {
+        ProcessParamModel processParamModel =
+            processParamApi.findByProcessSerialNumber(Y9LoginUserHolder.getTenantId(), processSerialNumber).getData();
+        if (processParamModel != null) {
+            processParamModel.setHostDeptId(deptId);
+            processParamModel.setHostDeptName(deptName);
+            processParamApi.saveOrUpdate(Y9LoginUserHolder.getTenantId(), processParamModel);
+        }
+        return Y9Result.success();
     }
 }
