9f01af49b0a2a0f8da7efd5433de2ff95a57be33
shidaobang <daobangshi@gmail.com>
2025-05-15 17:28:11 +0800

    fix: 经过反向代理的 base.url 可以不用特殊设置，只需要保证反向代理正确转发 X-Forwarded-* 等请求头

--- y9-module-jodconverter/risenet-y9boot-webapp-jodconverter/src/main/java/net/risesoft/config/WebConfig.java
+++ y9-module-jodconverter/risenet-y9boot-webapp-jodconverter/src/main/java/net/risesoft/config/WebConfig.java
@@ -3,10 +3,14 @@ package net.risesoft.config;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.servlet.DispatcherType;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -39,6 +43,23 @@ public class WebConfig implements WebMvcConfigurer {
         LOGGER.info("Add resource locations: {}", filePath);
         registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/",
             "classpath:/resources/", "classpath:/static/", "classpath:/public/", "file:" + filePath);
+    }
+
+    /**
+     * 针对经过反向代理的请求不能正确获得一些原始的请求信息，例如不能正确获得原始的 schema <br/>
+     * 此过滤器更多是为了减少外部 servlet 容器的配置 <br/>
+     *
+     * @return {@code FilterRegistrationBean<ForwardedHeaderFilter> }
+     * @see <a href= "https://docs.spring.io/spring-security/reference/servlet/appendix/proxy-server.html">Proxy Server
+     *      Configuration</a>
+     */
+    @Bean
+    FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+        ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+        FilterRegistrationBean<ForwardedHeaderFilter> registrationBean = new FilterRegistrationBean<>(filter);
+        registrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR);
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registrationBean;
     }
 
     @Bean
