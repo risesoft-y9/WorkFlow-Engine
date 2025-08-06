99d083f2882c517ae4088f36787b0c55d737604d
mengjuhua <mengjuhua@risesoft.net>
2025-08-06 17:30:33 +0800

    refactor: logback替换log4j2

--- y9-module-processadmin/risenet-y9boot-server-processadmin/build.gradle.kts
+++ y9-module-processadmin/risenet-y9boot-server-processadmin/build.gradle.kts
@@ -7,12 +7,7 @@ plugins {
 dependencies {
     providedRuntime(platform(libs.y9.digitalbase.dependencies))
 
-    api(project(":y9-module-processadmin:risenet-y9boot-support-processadmin")) {
-        // 排除项目依赖
-        exclude("org.springframework.boot", "spring-boot-starter-logging")
-        exclude("log4j", "log4j")
-        exclude("ch.qos.logback", "logback-core")
-    }
+    api(project(":y9-module-processadmin:risenet-y9boot-support-processadmin"))
     api("net.risesoft:risenet-y9boot-properties")
     api("net.risesoft:risenet-y9boot-starter-sso-oauth2-resource")
     api("net.risesoft:risenet-y9boot-common-tenant-datasource")
@@ -23,14 +18,8 @@ dependencies {
     api("net.risesoft:risenet-y9boot-api-feignclient-platform")
     api("net.risesoft:risenet-y9boot-starter-web")
     api("net.risesoft:risenet-y9boot-common-nacos")
-    api("org.springframework.boot:spring-boot-starter-validation") {
-        // 排除项目依赖
-        exclude("org.springframework.boot", "spring-boot-starter-logging")
-        exclude("ch.qos.logback", "logback-core")
-        exclude("ch.qos.logback", "logback-classic")
-    }
+    api("org.springframework.boot:spring-boot-starter-validation")
     api("org.springframework.boot:spring-boot-configuration-processor")
-    api("org.springframework.boot:spring-boot-starter-log4j2")
     api("org.apache.commons:commons-pool2")
     api(y9libs.mysql.connector.j)
     api(y9libs.dameng.dmdialect.hibernate62)
