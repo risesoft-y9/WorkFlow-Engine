f3c52a6ef16e182ffb36f7db9232608f334e2123
zhangchongjie <zhangchongjie@risesoft.net>
2025-08-06 10:55:28 +0800

    修改，办结流程保存数据中心
    (cherry picked from commit 669baa8109e42c066313722e88e4a984d6950d02)

--- y9-module-processadmin/risenet-y9boot-support-processadmin/src/main/java/net/risesoft/service/Process4CompleteUtilService.java
+++ y9-module-processadmin/risenet-y9boot-support-processadmin/src/main/java/net/risesoft/service/Process4CompleteUtilService.java
@@ -15,7 +15,7 @@ public interface Process4CompleteUtilService {
     void deleteDoneData(String processInstanceId);
 
     /**
-     * 保存办结数据到数据中心，用于办结件列表查询，截转年度数据
+     * 保存办结数据到ES，用于办结件列表查询，截转年度数据
      *
      * @param tenantId 租户id
      * @param year 年份
@@ -23,7 +23,7 @@ public interface Process4CompleteUtilService {
      * @param processInstanceId 流程实例id
      * @param personName 用户名称
      */
-    void saveToDataCenter(final String tenantId, final String year, final String userId, final String processInstanceId,
+    void saveToEs(final String tenantId, final String year, final String userId, final String processInstanceId,
         final String personName);
 
     /**
