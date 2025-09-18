200cebd5978674f10ef6eee20511319b362cd5e0
qinman <qinman@risesoft.net>
2025-09-18 16:04:42 +0800

    refactor: 代码优化

--- y9-module-itemadmin/risenet-y9boot-support-itemadmin/src/main/java/net/risesoft/service/config/ItemStartNodeRoleService.java
+++ y9-module-itemadmin/risenet-y9boot-support-itemadmin/src/main/java/net/risesoft/service/config/ItemStartNodeRoleService.java
@@ -14,114 +14,103 @@ import net.risesoft.model.platform.Role;
 public interface ItemStartNodeRoleService {
 
     /**
-     * Description:
-     *
-     * @param itemId
-     * @param processDefinitionId
+     * 
+     * @param itemId 事项id
+     * @param processDefinitionId 流程定义id
      */
     void copyBind(String itemId, String processDefinitionId);
 
     /**
-     * Description: 删除绑定路由节点角色信息
+     * 删除绑定路由节点角色信息
      *
-     * @param itemId
+     * @param itemId 事项id
      */
     void deleteBindInfo(String itemId);
 
     /**
-     * Description:
+     * 
      *
-     * @param id
-     * @return
+     * @param id 唯一标识
+     * @return ItemStartNodeRole
      */
     ItemStartNodeRole findById(String id);
 
     /**
-     * Description:
-     *
-     * @param itemId
-     * @param processDefinitionId
-     * @param taskDefKey
-     * @return
+     * 
+     * @param itemId 事项id
+     * @param processDefinitionId 流程定义id
+     * @param taskDefKey 任务节点key
+     * @return ItemStartNodeRole
      */
     ItemStartNodeRole findByItemIdAndProcessDefinitionIdAndTaskDefKey(String itemId, String processDefinitionId,
         String taskDefKey);
 
     /**
-     * 获取有权限的优先级最大的开始任务节点，如果都没有权限，则返回优先级最小的，，如果没有初始化权限，则返回第一个节点
+     * 获取有权限的优先级最大的开始任务节点，如果都没有权限，则返回优先级最小的，如果没有初始化权限，则返回第一个节点
      *
-     * @param itemId
-     * @return
+     * @param itemId 事项id
+     * @return String - 开始节点key
      */
     String getStartTaskDefKey(String itemId);
 
     /**
      * 获取有权限的所有任务节点
      *
-     * @param itemId
-     * @return
+     * @param itemId 事项id
+     * @return List<ItemStartNodeRoleModel>
      */
     List<ItemStartNodeRoleModel> getAllStartTaskDefKey(String itemId);
 
     /**
-     * Description:
-     *
-     * @param itemId
-     * @param processDefinitionId
-     * @param taskDefKey
-     * @param taskDefName
+     * 
+     * @param itemId 事项id
+     * @param processDefinitionId 流程定义id
+     * @param taskDefKey 任务节点key
+     * @param taskDefName 任务节点名称
      */
     void initRole(String itemId, String processDefinitionId, String taskDefKey, String taskDefName);
 
     /**
-     * Description:
-     *
-     * @param itemId
-     * @param processDefinitionId
-     * @return
+     * 
+     * @param itemId 事项id
+     * @param processDefinitionId 流程定义id
+     * @return List<ItemStartNodeRole>
      */
     List<ItemStartNodeRole> listByItemIdAndProcessDefinitionId(String itemId, String processDefinitionId);
 
     /**
-     * Description:
-     *
-     * @param itemId
-     * @param processDefinitionId
-     * @param taskDefKey
-     * @return
+     * 
+     * @param itemId 事项id
+     * @param processDefinitionId 流程定义id
+     * @param taskDefKey 任务节点key
+     * @return List<Role>
      */
     List<Role> listRoleByItemIdAndProcessDefinitionIdAndTaskDefKey(String itemId, String processDefinitionId,
         String taskDefKey);
 
     /**
-     * Description:
-     *
-     * @param itemId
-     * @param processDefinitionId
-     * @param taskDefKey
-     * @param roleIds
+     * 
+     * @param itemId 事项id
+     * @param processDefinitionId 流程定义id
+     * @param taskDefKey 任务节点key
+     * @param roleIds 角色id
      */
     void removeRole(String itemId, String processDefinitionId, String taskDefKey, String roleIds);
 
     /**
+     * 
      * 保存排序
-     *
-     * @param idAndTabIndexs
-     */
-    /**
-     * Description:
-     *
-     * @param idAndTabIndexs
+     * 
+     * @param idAndTabIndexs idAndTabIndexs
      */
     void saveOrder(String[] idAndTabIndexs);
 
     /**
-     * Description:
-     *
-     * @param itemId
-     * @param processDefinitionId
-     * @param taskDefKey
-     * @param roleIds
+     * 
+     * @param itemId 事项id
+     * @param processDefinitionId 流程定义id
+     * @param taskDefKey 任务节点key
+     * @param roleIds 角色id
      */
     void saveRole(String itemId, String processDefinitionId, String taskDefKey, String roleIds);
 }
