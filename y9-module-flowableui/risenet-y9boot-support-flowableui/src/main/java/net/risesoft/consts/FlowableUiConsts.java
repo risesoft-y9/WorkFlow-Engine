28a91240a124be7e787e2fa87b8deb200167821e
yihong <Yh599598!@#>
2025-10-16 15:19:02 +0800

    代码扫码问题修复
    (cherry picked from commit 37d067e4ae8d464e1622427e3f3ff4ee47b8fe14)

--- y9-module-flowableui/risenet-y9boot-support-flowableui/src/main/java/net/risesoft/consts/FlowableUiConsts.java
+++ y9-module-flowableui/risenet-y9boot-support-flowableui/src/main/java/net/risesoft/consts/FlowableUiConsts.java
@@ -8,6 +8,22 @@ public class FlowableUiConsts {
 
     public static final String ISZHUBAN = "isZhuBan";
 
+    public static final String PROCESSDEFINITIONID_KEY = "processDefinitionId";
+    public static final String PROCESSINSTANCEID_KEY = "processInstanceId";
+    public static final String ITEMNAME_KEY = "itemName";
+    public static final String PROCESSDEFINITIONKEY_KEY = "processDefinitionKey";
+    public static final String SERIALNUMBER_KEY = "serialNumber";
+    public static final String STARTTIME_KEY = "startTime";
+    public static final String ENDTIME_KEY = "endTime";
+    public static final String TASKDEFINITIONKEY_KEY = "taskDefinitionKey";
+    public static final String TASKASSIGNEE_KEY = "taskAssignee";
+    public static final String CREATEUSERNAME_KEY = "creatUserName";
+    public static final String ITEMID_KEY = "itemId";
+    public static final String LEVEL_KEY = "level";
+    public static final String NUMBER_KEY = "number";
+    public static final String ITEMBOX_KEY = "itembox";
+    public static final String TASKID_KEY = "taskId";
+
     /**
      * 私有构造函数，防止实例化
      */
