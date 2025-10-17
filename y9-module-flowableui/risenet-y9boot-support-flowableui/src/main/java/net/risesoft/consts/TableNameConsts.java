4988205b0571fb170a3d382042531c05c54bce1e
qinman <qinman@risesoft.net>
2025-10-17 17:04:49 +0800

    refactor: 代码优化

---
+++ y9-module-flowableui/risenet-y9boot-support-flowableui/src/main/java/net/risesoft/consts/TableNameConsts.java
@@ -0,0 +1,9 @@
+package net.risesoft.consts;
+
+import java.util.List;
+
+public class TableNameConsts {
+
+    public static List<String> ACT_HI_TABLES =
+        List.of("ACT_HI_TASKINST", "ACT_HI_VARINST", "ACT_HI_IDENTITYLINK", "ACT_HI_ACTINST", "ACT_HI_PROCINST");
+}
