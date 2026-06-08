539e97fbf4097e72de83ebf2fa1f82b3031f7572
zhangchongjie <zhangchongjie@risesoft.net>
2026-06-08 14:08:43 +0800

    SignaturePictureApi接口优化
    (cherry picked from commit 9614a5b5c5193af16c3e6283330661a75f3ab70d)

--- y9-module-itemadmin/risenet-y9boot-api-interface-itemadmin/src/main/java/net/risesoft/api/itemadmin/SignaturePictureApi.java
+++ y9-module-itemadmin/risenet-y9boot-api-interface-itemadmin/src/main/java/net/risesoft/api/itemadmin/SignaturePictureApi.java
@@ -19,47 +19,40 @@ public interface SignaturePictureApi {
     /**
      * 删除签名图片
      *
-     * @param tenantId 租户id
      * @param id id
      * @return {@code Y9Result<Object>} 通用请求返回对象
      * @since 9.6.6
      */
     @PostMapping("/deleteById")
-    Y9Result<Object> deleteById(@RequestParam("tenantId") String tenantId, @RequestParam("id") String id);
+    Y9Result<Object> deleteById(@RequestParam("id") String id);
 
     /**
      * 根据id获取签名图片
      *
-     * @param tenantId 租户id
      * @param id id
      * @return {@code Y9Result<SignaturePictureModel>} 通用请求返回对象 - data 是签名图片
      * @since 9.6.6
      */
     @GetMapping("/findById")
-    Y9Result<SignaturePictureModel> findById(@RequestParam("tenantId") String tenantId, @RequestParam("id") String id);
+    Y9Result<SignaturePictureModel> findById(@RequestParam("id") String id);
 
     /**
      * 根据人员id获取签名图片
      *
-     * @param tenantId 租户id
      * @param userId 人员id
      * @return {@code Y9Result<SignaturePictureModel>} 通用请求返回对象 - data 是签名图片
      * @since 9.6.6
      */
     @GetMapping("/findByUserId")
-    Y9Result<SignaturePictureModel> findByUserId(@RequestParam("tenantId") String tenantId,
-        @RequestParam("userId") String userId);
+    Y9Result<SignaturePictureModel> findByUserId(@RequestParam("userId") String userId);
 
     /**
      * 保存或更新签名图片
      *
-     * @param tenantId 租户id
-     * @param userId 人员id
      * @param spJson spJson
      * @return {@code Y9Result<SignaturePictureModel>} 通用请求返回对象 - data 是签名图片
      * @since 9.6.6
      */
     @PostMapping("/saveOrUpdate")
-    Y9Result<SignaturePictureModel> saveOrUpdate(@RequestParam("tenantId") String tenantId,
-        @RequestParam("userId") String userId, @RequestParam("spJson") String spJson);
+    Y9Result<SignaturePictureModel> saveOrUpdate(@RequestParam("spJson") String spJson);
 }
