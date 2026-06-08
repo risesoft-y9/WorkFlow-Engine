4244bcaefcc0f9149102d9ddcae2ecd53ce85372
zhangchongjie <zhangchongjie@risesoft.net>
2026-06-08 10:34:47 +0800

    SpeakInfoApi接口优化

--- y9-module-itemadmin/risenet-y9boot-api-interface-itemadmin/src/main/java/net/risesoft/api/itemadmin/SpeakInfoApi.java
+++ y9-module-itemadmin/risenet-y9boot-api-interface-itemadmin/src/main/java/net/risesoft/api/itemadmin/SpeakInfoApi.java
@@ -23,63 +23,50 @@ public interface SpeakInfoApi {
     /**
      * 逻辑删除发言信息
      *
-     * @param tenantId 租户id
-     * @param userId 人员id
      * @param id 主键id
      * @return {@code Y9Result<Object>} 通用请求返回对象
      * @since 9.6.6
      */
     @PostMapping(value = "/deleteById")
-    Y9Result<Object> deleteById(@RequestParam("tenantId") String tenantId, @RequestParam("userId") String userId,
-        @RequestParam("id") String id);
+    Y9Result<Object> deleteById(@RequestParam("id") String id);
 
     /**
      * 根据唯一标示获取发言信息
      *
-     * @param tenantId 租户id
      * @param id 主键id
      * @return {@code Y9Result<SpeakInfoModel>} 通用请求返回对象 - data 是发言信息
      * @since 9.6.6
      */
     @GetMapping(value = "/findById")
-    Y9Result<SpeakInfoModel> findById(@RequestParam("tenantId") String tenantId, @RequestParam("id") String id);
+    Y9Result<SpeakInfoModel> findById(@RequestParam("id") String id);
 
     /**
      * 根据流程实例查找某一个流程的所有发言信息，根据时间倒叙排列
      *
-     * @param tenantId 租户id
-     * @param userId 人员id
      * @param processInstanceId 流程实例id
      * @return {@code Y9Result<List<SpeakInfoModel>>} 通用请求返回对象 - data 是发言信息列表
      * @since 9.6.6
      */
     @GetMapping(value = "/findByProcessInstanceId")
-    Y9Result<List<SpeakInfoModel>> findByProcessInstanceId(@RequestParam("tenantId") String tenantId,
-        @RequestParam("userId") String userId, @RequestParam("processInstanceId") String processInstanceId);
+    Y9Result<List<SpeakInfoModel>> findByProcessInstanceId(@RequestParam("processInstanceId") String processInstanceId);
 
     /**
      * 获取未读消息计数
      *
-     * @param tenantId 租户id
-     * @param userId 人员id
      * @param processInstanceId 流程实例id
      * @return {@code Y9Result<Integer>} 通用请求返回对象 - data 是未读消息计数
      * @since 9.6.6
      */
     @GetMapping(value = "/getNotReadCount")
-    Y9Result<Integer> getNotReadCount(@RequestParam("tenantId") String tenantId, @RequestParam("userId") String userId,
-        @RequestParam("processInstanceId") String processInstanceId);
+    Y9Result<Integer> getNotReadCount(@RequestParam("processInstanceId") String processInstanceId);
 
     /**
      * 保存或者更新发言信息
      *
-     * @param tenantId 租户id
-     * @param userId 人员id
      * @param speakInfoModel 发言信息
      * @return {@code Y9Result<String>} 通用请求返回对象 - data 是发言ID
      * @since 9.6.6
      */
     @PostMapping(value = "/saveOrUpdate", consumes = MediaType.APPLICATION_JSON_VALUE)
-    Y9Result<String> saveOrUpdate(@RequestParam("tenantId") String tenantId, @RequestParam("userId") String userId,
-        @RequestBody SpeakInfoModel speakInfoModel);
+    Y9Result<String> saveOrUpdate(@RequestBody SpeakInfoModel speakInfoModel);
 }
