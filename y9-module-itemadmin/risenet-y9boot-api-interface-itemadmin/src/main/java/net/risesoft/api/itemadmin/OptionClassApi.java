379b6e83d294b852ced46bc6646397f219fc5133
zhangchongjie <zhangchongjie@risesoft.net>
2026-06-08 17:30:39 +0800

    OptionClassApi接口优化
    (cherry picked from commit 6a038ab3a82aaba83f41b5f4d32f48b4f4676852)

--- y9-module-itemadmin/risenet-y9boot-api-interface-itemadmin/src/main/java/net/risesoft/api/itemadmin/OptionClassApi.java
+++ y9-module-itemadmin/risenet-y9boot-api-interface-itemadmin/src/main/java/net/risesoft/api/itemadmin/OptionClassApi.java
@@ -21,22 +21,20 @@ public interface OptionClassApi {
      * 获取数据字典列表
      *
      * @param tenantId 租户id
-     * @param type 字典标识
-     * @return {@code Y9Result<List<Y9FormOptionValueModel>>} 通用请求返回对象 -data是数据字典列表
-     * @since 9.6.6
-     */
-    @GetMapping(value = "/getOptionValueList")
-    Y9Result<List<Y9FormOptionValueModel>> getOptionValueList(@RequestParam("tenantId") String tenantId,
-        @RequestParam("type") String type);
-
-    /**
-     * 获取数据字典列表
-     *
-     * @param tenantId 租户id
      * @return {@code Y9Result<List<Y9FormOptionValueModel>>} 通用请求返回对象 -data是数据字典列表
      * @since 9.6.6
      */
     @GetMapping(value = "/findAll")
     Y9Result<List<Y9FormOptionValueModel>> findAll(@RequestParam("tenantId") String tenantId);
+
+    /**
+     * 获取数据字典列表
+     *
+     * @param type 字典标识
+     * @return {@code Y9Result<List<Y9FormOptionValueModel>>} 通用请求返回对象 -data是数据字典列表
+     * @since 9.6.6
+     */
+    @GetMapping(value = "/getOptionValueList")
+    Y9Result<List<Y9FormOptionValueModel>> getOptionValueList(@RequestParam("type") String type);
 
 }
