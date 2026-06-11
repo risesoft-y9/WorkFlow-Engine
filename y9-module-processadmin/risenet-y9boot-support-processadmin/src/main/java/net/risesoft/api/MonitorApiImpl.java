92f6a36af54cba20dec7049ea6d44bcf09f4bba2
zhangchongjie <zhangchongjie@risesoft.net>
2026-06-11 10:05:32 +0800

    MonitorApi接口优化

--- y9-module-processadmin/risenet-y9boot-support-processadmin/src/main/java/net/risesoft/api/MonitorApiImpl.java
+++ y9-module-processadmin/risenet-y9boot-support-processadmin/src/main/java/net/risesoft/api/MonitorApiImpl.java
@@ -14,7 +14,6 @@ import net.risesoft.pojo.Y9Page;
 import net.risesoft.pojo.Y9Result;
 import net.risesoft.service.CustomMonitorService;
 import net.risesoft.service.CustomRecycleService;
-import net.risesoft.y9.FlowableTenantInfoHolder;
 
 /**
  * 监控流程实例接口
@@ -33,38 +32,47 @@ public class MonitorApiImpl implements MonitorApi {
     private final CustomRecycleService customRecycleService;
 
     /**
+     * 根据系统名称获取监控在办件
+     *
+     * @param systemName 系统英文名称
+     * @param page 页码
+     * @param rows 行数
+     * @return {@code Y9Page<HistoricProcessInstanceModel>} 通用请求返回对象 - rows 在办件列表
+     * @since 9.6.6
+     */
+    @Override
+    public Y9Page<HistoricProcessInstanceModel> getAllListBySystemName(@RequestParam String systemName,
+        @RequestParam Integer page, @RequestParam Integer rows) {
+        return customMonitorService.pageDoingListBySystemName(systemName, page, rows);
+    }
+
+    /**
      * 根据流程定义Key获取监控在办件统计数量
      *
-     * @param tenantId 租户Id
      * @param processDefinitionKey 流程定义Key
      * @return {@code Y9Result<Long>} 通用请求返回对象 - data 在办件数量
      * @since 9.6.6
      */
     @Override
-    public Y9Result<Long> getDoingCountByProcessDefinitionKey(@RequestParam String tenantId,
-        @RequestParam String processDefinitionKey) {
-        FlowableTenantInfoHolder.setTenantId(tenantId);
+    public Y9Result<Long> getDoingCountByProcessDefinitionKey(@RequestParam String processDefinitionKey) {
         return Y9Result.success(customMonitorService.getDoingCountByProcessDefinitionKey(processDefinitionKey));
     }
 
     /**
      * 根据系统英文名称获取监控在办件数量
      *
-     * @param tenantId 租户Id
      * @param systemName 系统英文名称
      * @return {@code Y9Result<Long>} 通用请求返回对象 - data 在办件数量
      * @since 9.6.6
      */
     @Override
-    public Y9Result<Long> getDoingCountBySystemName(@RequestParam String tenantId, @RequestParam String systemName) {
-        FlowableTenantInfoHolder.setTenantId(tenantId);
+    public Y9Result<Long> getDoingCountBySystemName(@RequestParam String systemName) {
         return Y9Result.success(customMonitorService.getDoingCountBySystemName(systemName));
     }
 
     /**
      * 根据流程定义Key获取监控在办件
      *
-     * @param tenantId 租户Id
      * @param processDefinitionKey 流程定义Key
      * @param page 页码
      * @param rows 行数
@@ -73,16 +81,14 @@ public class MonitorApiImpl implements MonitorApi {
      */
     @Override
     @GetMapping(value = "/getDoingListByProcessDefinitionKey")
-    public Y9Page<HistoricProcessInstanceModel> getDoingListByProcessDefinitionKey(@RequestParam String tenantId,
+    public Y9Page<HistoricProcessInstanceModel> getDoingListByProcessDefinitionKey(
         @RequestParam String processDefinitionKey, @RequestParam Integer page, @RequestParam Integer rows) {
-        FlowableTenantInfoHolder.setTenantId(tenantId);
         return customMonitorService.pageDoingListByProcessDefinitionKey(processDefinitionKey, page, rows);
     }
 
     /**
      * 根据系统名称获取监控在办件
      *
-     * @param tenantId 租户Id
      * @param systemName 系统英文名称
      * @param page 页码
      * @param rows 行数
@@ -90,48 +96,26 @@ public class MonitorApiImpl implements MonitorApi {
      * @since 9.6.6
      */
     @Override
-    public Y9Page<HistoricProcessInstanceModel> getDoingListBySystemName(@RequestParam String tenantId,
-        @RequestParam String systemName, @RequestParam Integer page, @RequestParam Integer rows) {
-        FlowableTenantInfoHolder.setTenantId(tenantId);
-        return customMonitorService.pageDoingListBySystemName(systemName, page, rows);
-    }
-
-    /**
-     * 根据系统名称获取监控在办件
-     *
-     * @param tenantId 租户Id
-     * @param systemName 系统英文名称
-     * @param page 页码
-     * @param rows 行数
-     * @return {@code Y9Page<HistoricProcessInstanceModel>} 通用请求返回对象 - rows 在办件列表
-     * @since 9.6.6
-     */
-    @Override
-    public Y9Page<HistoricProcessInstanceModel> getAllListBySystemName(@RequestParam String tenantId,
-        @RequestParam String systemName, @RequestParam Integer page, @RequestParam Integer rows) {
-        FlowableTenantInfoHolder.setTenantId(tenantId);
+    public Y9Page<HistoricProcessInstanceModel> getDoingListBySystemName(@RequestParam String systemName,
+        @RequestParam Integer page, @RequestParam Integer rows) {
         return customMonitorService.pageDoingListBySystemName(systemName, page, rows);
     }
 
     /**
      * 根据流程定义Key获取监控办结件统计数量
      *
-     * @param tenantId 租户Id
      * @param processDefinitionKey 流程定义Key
      * @return {@code Y9Result<Long>} 通用请求返回对象 - data 办结件统计数量
      * @since 9.6.6
      */
     @Override
-    public Y9Result<Long> getDoneCountByProcessDefinitionKey(@RequestParam String tenantId,
-        @RequestParam String processDefinitionKey) {
-        FlowableTenantInfoHolder.setTenantId(tenantId);
+    public Y9Result<Long> getDoneCountByProcessDefinitionKey(@RequestParam String processDefinitionKey) {
         return Y9Result.success(customMonitorService.getDoneCountByProcessDefinitionKey(processDefinitionKey));
     }
 
     /**
      * 根据流程定义Key和其他条件搜索在办件
      *
-     * @param tenantId 租户id
      * @param processDefinitionKey 流程定义Key
      * @param searchTerm 搜索词
      * @param page 页码
@@ -140,17 +124,15 @@ public class MonitorApiImpl implements MonitorApi {
      * @since 9.6.6
      */
     @Override
-    public Y9Page<HistoricProcessInstanceModel> searchDoingListByProcessDefinitionKey(@RequestParam String tenantId,
+    public Y9Page<HistoricProcessInstanceModel> searchDoingListByProcessDefinitionKey(
         @RequestParam String processDefinitionKey, @RequestParam String searchTerm, @RequestParam Integer page,
         @RequestParam Integer rows) {
-        FlowableTenantInfoHolder.setTenantId(tenantId);
         return customMonitorService.searchDoingListByProcessDefinitionKey(processDefinitionKey, searchTerm, page, rows);
     }
 
     /**
      * 根据系统英文名称和其他条件搜索在办件
      *
-     * @param tenantId 租户id
      * @param systemName 系统英文名称
      * @param searchTerm 搜索词
      * @param page 页码
@@ -159,10 +141,8 @@ public class MonitorApiImpl implements MonitorApi {
      * @since 9.6.6
      */
     @Override
-    public Y9Page<HistoricProcessInstanceModel> searchDoingListBySystemName(@RequestParam String tenantId,
-        @RequestParam String systemName, @RequestParam String searchTerm, @RequestParam Integer page,
-        @RequestParam Integer rows) {
-        FlowableTenantInfoHolder.setTenantId(tenantId);
+    public Y9Page<HistoricProcessInstanceModel> searchDoingListBySystemName(@RequestParam String systemName,
+        @RequestParam String searchTerm, @RequestParam Integer page, @RequestParam Integer rows) {
         return customMonitorService.searchDoingListBySystemName(systemName, searchTerm, page, rows);
     }
 }
