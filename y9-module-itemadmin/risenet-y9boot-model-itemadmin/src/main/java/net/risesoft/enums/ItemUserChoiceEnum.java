8a7bcfc122265d1c38d525a3ac714453c43cd878
qinman <qinman@risesoft.net>
2026-06-11 14:15:46 +0800

    feat: 用户选择的发送对象枚举
    (cherry picked from commit 954a54c244fec8cb98aa148e326d8384b4355faf)

--- y9-module-itemadmin/risenet-y9boot-model-itemadmin/src/main/java/net/risesoft/enums/ItemUserChoiceEnum.java
+++ y9-module-itemadmin/risenet-y9boot-model-itemadmin/src/main/java/net/risesoft/enums/ItemUserChoiceEnum.java
@@ -12,12 +12,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ItemUserChoiceEnum implements ValuedEnum<Integer> {
-    /** 角色 */
-    ROLE(1, "角色"),
     /** 部门 */
     DEPARTMENT(2, "部门"),
-    /**  */
-    ROLE_DYNAMIC(4, "动态角色"),
     /**  */
     POSITION(6, "岗位"),
     /**  */
