fef845cb97f95d1c93f0b7a7a9e21729b1c15849
shidaobang <shidaobang@risesoft.net>
2026-07-23 15:17:36 +0800

    fix: jpa 及 jpaVendor 相关配置属性的优先级对齐 Spring Boot

--- y9-module-itemadmin/risenet-y9boot-server-itemadmin/src/main/java/net/risesoft/config/ItemAdminConfiguration.java
+++ y9-module-itemadmin/risenet-y9boot-server-itemadmin/src/main/java/net/risesoft/config/ItemAdminConfiguration.java
@@ -1,14 +1,11 @@
 package net.risesoft.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.orm.jpa.JpaVendorAdapter;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import lombok.extern.slf4j.Slf4j;
@@ -32,12 +29,6 @@ import feign.RequestInterceptor;
 @EnableKafka
 @Slf4j
 public class ItemAdminConfiguration {
-
-    @Bean
-    @ConditionalOnMissingBean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        return new HibernateJpaVendorAdapter();
-    }
 
     @Bean
     public Y9Context y9Context() {
