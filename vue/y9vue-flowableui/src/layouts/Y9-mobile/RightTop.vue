d9899cce01d45ca05fc81a63d0e8987612f45355
mengjuhua <mengjuhua@risesoft.net>
2026-09-09 17:28:12 +0800

    refactor：重构白天黑夜功能

--- vue/y9vue-flowableui/src/layouts/Y9-mobile/RightTop.vue
+++ vue/y9vue-flowableui/src/layouts/Y9-mobile/RightTop.vue
@@ -1,7 +1,10 @@
 <script lang="ts" setup>
     import { inject } from 'vue';
-    import RightTopUser from '../components/RightTopUser.vue';
     import { useSettingStore } from '@/store/modules/settingStore';
+
+    import RightTopUser from '../components/RightTopUser.vue';
+    import UseDark from '../components/UseDark/index.vue';
+
     // 注入 字体对象
     const fontSizeObj: any = inject('sizeObjInfo');
     const props = defineProps({
@@ -21,14 +24,6 @@
     const toggleCollapsedFunc = () => {
         toggleCollapsed();
     };
-
-    // 白天黑夜功能
-    const isDark = useDark({
-        selector: 'html',
-        valueDark: 'theme-dark',
-        valueLight: ''
-    });
-    const toggleDark = useToggle(isDark);
 
     // 锁屏
     const lockScreenFunc = () => {
@@ -77,11 +72,8 @@
             <!-- <div class="item notify">
                 <el-badge :value="3" class="badge"></el-badge>
                 <i class="ri-notification-line"></i>
-            </div>
-            <div class="item isDark">
-                <i class="ri-moon-line" @click="toggleDark" v-if="!isDark"></i>
-                <i class="ri-sun-line" @click="toggleDark" v-else></i>
             </div> -->
+            <!-- <UseDark /> -->
             <div :class="{ item: true, user: true, 'user-mobile': settingStore.getWindowWidth > 425 }">
                 <RightTopUser />
             </div>
--- vue/y9vue-itemadmin/src/layouts/Y9-default/RightTop.vue
+++ vue/y9vue-itemadmin/src/layouts/Y9-default/RightTop.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
-    import RightTopUser from '../components/RightTopUser.vue';
     import { useSettingStore } from '@/store/modules/settingStore';
     import y9_storage from '@/utils/storage';
+
+    import RightTopUser from '../components/RightTopUser.vue';
+    import UseDark from '../components/UseDark/index.vue';
     // 个人信息 —— 头像
     const userInfo = y9_storage.getObjectItem('ssoUserInfo');
 
@@ -24,14 +26,6 @@
             menuCollapsed: !settingStore.getMenuCollapsed
         });
     };
-
-    // 白天黑夜功能
-    const isDark = useDark({
-        selector: 'html',
-        valueDark: 'theme-dark',
-        valueLight: ''
-    });
-    const toggleDark = useToggle(isDark);
 
     // 锁屏
     const lockScreenFunc = () => {
@@ -83,15 +77,11 @@
                 <el-badge :value="3" class="badge"></el-badge>
                 <i class="ri-notification-line"></i>
             </div> -->
-            <!-- <div class="item isDark">
-                <i class="ri-moon-line" @click="toggleDark" v-if="!isDark"></i>
-                <i class="ri-sun-line" @click="toggleDark" v-else></i>
-            </div> -->
+            <!-- <UseDark /> -->
             <div class="item user">
                 <RightTopUser />
             </div>
             <div class="item user">
-                <!-- 头像测试链接地址：https://www.youshengyun.com/fileManager/files/e6b5d41fd2bd4cdda538139f9b7848c7.jpg -->
                 <el-avatar :src="userInfo.avator ? userInfo.avator : ''"> {{ $t(`${userInfo.loginName}`) }}</el-avatar>
             </div>
             <!-- <div class="item" @click="logout">
